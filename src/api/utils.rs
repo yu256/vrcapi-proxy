1f3bfa9bdbc3a5fb8cc585b2b560a71411430537
yu256 <122450013+yu256@users.noreply.github.com>
2023-08-14 16:14:25 +0900

    refactor

--- src/api/utils.rs
+++ src/api/utils.rs
@@ -11,13 +11,13 @@ pub(crate) async fn request(
     method: reqwest::Method,
     target: &str,
     cookie: &str,
-) -> anyhow::Result<Response> {
-    Ok(CLIENT
+) -> Result<Response, reqwest::Error> {
+    CLIENT
         .request(method, target)
         .header(UA, UA_VALUE)
         .header(COOKIE, cookie)
         .send()
-        .await?)
+        .await
 }
 
 pub(crate) trait StrExt {
