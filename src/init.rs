db37ed7bcfb44936553b5f5c24aec16cec9a2a1b
yu256 <122450013+yu256@users.noreply.github.com>
2023-10-28 13:09:51 +0900

    data.jsonとconfig.jsonの判定を分けるように

--- src/init.rs
+++ src/init.rs
@@ -10,23 +10,27 @@ pub(crate) struct Config {
 }
 
 pub(crate) fn init() -> Result<()> {
-    if DATA_PATH.join("data.json").is_file() && DATA_PATH.join("config.json").is_file() {
+    let is_data_exist = DATA_PATH.join("data.json").is_file();
+    let is_config_exist = DATA_PATH.join("config.json").is_file();
+
+    if is_config_exist && is_data_exist {
         return Ok(());
     }
 
-    let conf = Config {
-        listen: "0.0.0.0:8000".into(),
-        cors: "http://localhost:3000".into(),
-    };
-    let data: HashMap<String, String> = HashMap::new();
+    if !is_data_exist {
+        let data: HashMap<String, String> = HashMap::new();
+        write_json(&data, "data")?;
+    }
 
-    write_json(&conf, "config")?;
-    write_json(&data, "data")?;
+    if !is_config_exist {
+        let conf = Config {
+            listen: "0.0.0.0:8000".into(),
+            cors: "http://localhost:3000".into(),
+        };
+        write_json(&conf, "config")?;
+    }
 
-    println!(
-        "{}にコンフィグファイルを生成しました。",
-        DATA_PATH.display()
-    );
+    println!("{}にjsonを生成しました。", DATA_PATH.display());
 
     std::process::exit(0);
 }
